350e01dd58f511da6c6946aac49f31e1601e61e7
MarkGhann <gmarkmg@yandex.ru>
2021-11-21 06:49:37 +0300

    08_Regexps: initial commit

---
+++ 08_Regexps/README.md
@@ -0,0 +1,21 @@
+# Task 8: Regexps
+
+## Cmake
+```
+cd <build dir>
+cmake <source dir> [-DREADLINE_ENABLED=true/false]
+```
+
+## Build and Run tests
+```
+make run_tests
+```
+## Build rhasher
+```
+make esub
+```
+
+## Clean (Delete All Related Files from  Build Directory)
+```
+make build_clean
+```
---
+++ 08_Regexps/esub.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <regex.h>
+
+#define PM 4
+
+void usage()
+{
+    fprintf(stderr, "Usage:\n\t ./esub <regexp> <substitution> <string>\n\n");
+}
+
+int parseArgs(int argc)
+{
+    int ARGS_NUM = 3;
+
+    if (argc != ARGS_NUM) {
+        return -1;
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+
+    if (parseArgs(argc) != 0) {
+        return 1;
+    }
+
+    size_t number = 0;
+    char *line=NULL;
+
+    regex_t re;
+    regmatch_t pm[PM];
+
+    regcomp(&re, argv[1], 0);
+
+    for(; getline(&line, &number, stdin); number=0, line=NULL) {
+        if (!regexec(&re, line, PM, pm, 0)) {
+            for(int i = 0; i < PM; i++) {
+                printf("%d: %d/%d\n", i, pm[i].rm_so, pm[i].rm_eo);
+            }
+        }
+        free(line);
+    }
+
+    regfree(&re);
+
+    return 0;
+}
